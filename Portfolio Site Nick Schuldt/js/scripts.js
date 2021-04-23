1898de552f84a2663eeab2032f1c4189689ed36c
NickSchuldt <80654008+NickSchuldt@users.noreply.github.com>
2021-04-23 11:07:19 -0400

    Making Public Repository

---
+++ Portfolio Site Nick Schuldt/js/scripts.js
@@ -0,0 +1,10 @@
+console.log('Hey! Welcome to My Porfolio Site')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
